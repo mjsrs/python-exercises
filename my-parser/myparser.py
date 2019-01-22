ad1aa71f0092f1c30899f3c85f9fdd9e90ec59f6
mjsrs <mjsrs@sapo.pt>
2019-01-22 00:38:49 +0000

    code style fix

--- my-parser/myparser.py
+++ my-parser/myparser.py
@@ -7,9 +7,12 @@ from utils import TextParser
 def newFileName(file_name):
     # isolate file extension
     # reverse string name and split only one occurrency
-    arr = file_name[::-1].split('.' ,1)
-    #re-reverse file name; add time; add file extension
-    return '%s_%s.%s' % (arr[1][::-1], datetime.datetime.now().strftime('%H%M%S'), arr[0])
+    arr = file_name[::-1].split('.', 1)
+    # re-reverse file name; add time; add file extension
+    return '%s_%s.%s' % (arr[1][::-1],
+                         datetime.datetime.now().strftime('%H%M%S'),
+                         arr[0])
+
 
 @click.command()
 @click.argument('fname', type=click.Path(exists=True))
@@ -20,4 +23,3 @@ def cli(fname):
             text = TextParser(article, REPLACEMENTS)
             for line in text:
                 new_article.write(line)
-
